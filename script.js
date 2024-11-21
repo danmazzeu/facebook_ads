05c75a768521a35dd5058422f02959eff364bc89
Daniel Mazzeu <danmazzeu9@gmail.com>
2024-11-21 13:05:41 -0300

    Update

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ const url = "https://melhorenvio.com.br/p/NqJnd8uDqy";
 let timeLeft = 3;
 
 function updateCountdown() {
-    timeLeft--;
-    timeElement.textContent = timeLeft;
-
     if (timeLeft <= 0) {
         window.location.href = url;
     }
+    
+    timeLeft--;
+    timeElement.textContent = timeLeft;
 }
 
 const countdownInterval = setInterval(updateCountdown, 1000);
